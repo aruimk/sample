66d0cf85195a858cbeea11f1094d9c5fa56bb6e2
aruimk <miurakimihiro@gmail.com>
2018-01-25 16:28:10 +0900

    make getfilename.py

--- getfilename.py
+++ getfilename.py
@@ -1,3 +1,15 @@
 """
 指定したパスにあるファイルのファイル名を取得するプログラム
 """
+
+import os
+
+fw = open('/Users/*******/outputfile.txt','w')
+PATH = '/Users/*********************' # ファイルネームを取得したいパス
+
+
+list_filename = os.listdir(PATH)
+#print(list_filename)
+
+for i in range(len(list_filename)):
+    print(PATH + '/' + list_filename[i])
